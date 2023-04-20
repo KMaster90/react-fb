bd2d09f62d88a9a4f68eca58652c5f64a533d587
KMaster90 <73232282+KMaster90@users.noreply.github.com>
2023-04-20 02:15:45 +0200

    Intro: React, TypeScript e TSX

---
+++ react-intro/src/components/empty.tsx
@@ -0,0 +1,4 @@
+export const Empty = () =>
+    <>
+        <em>No items</em>
+    </>
---
+++ react-intro/src/components/total.tsx
@@ -0,0 +1,4 @@
+interface TotalProps {
+    total: number;
+}
+export const Total =  ({ total }: TotalProps) => <>There are {total} products</>;
